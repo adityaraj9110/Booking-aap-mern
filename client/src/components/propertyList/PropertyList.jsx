8e2285f04f4a417dfdde0d4228c759d3cce20100
Aditya raj <80067523+adityaraj9110@users.noreply.github.com>
2023-05-16 02:14:04 +0530

    Update PropertyList.jsx
    INSTEAD OF APPLY MAP ON IMAGES APPLY MAP ON DATA ONLY THIS WILL PREVENT YOU FROM UNDEFINED ERROR

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,8 +1,11 @@
-import useFetch from "../hookes/useFetch";
+import React from "react";
+import useFetch from "../../Hookes/useFetch";
 import "./propertyList.css";
 
 const PropertyList = () => {
-  const { data, loading, error } = useFetch("hotels/countByType");
+  const { data, loading, error } = useFetch(
+    "http://localhost:8800/api/hotels/countByType"
+  );
 
   const images = [
     "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
@@ -11,6 +14,7 @@ const PropertyList = () => {
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
   ];
+
   return (
     <div className="pList">
       {loading ? (
@@ -18,16 +22,14 @@ const PropertyList = () => {
       ) : (
         <>
           {data &&
-            images.map((img,i) => (
+            data.map((item, i) => (
               <div className="pListItem" key={i}>
-                <img
-                  src={img}
-                  alt=""
-                  className="pListImage"
-                />
-                <div className="pListTitle">
-                  <h1>{data[i]?.type}</h1>
-                  <h2>{data[i]?.count} {data[i]?.type}</h2>
+                <img src={images[i]} alt="" className="pListImg" />
+                <div className="pListTitles">
+                  <h1>{item.type}</h1>
+                  <h2>
+                    {item.count} {item.type}
+                  </h2>
                 </div>
               </div>
             ))}
